a70f4ceb0ad22dfe1f70a6c8c9662624250c1952
estebanll2118-jpg <estebanll2118@gmail.com>
2026-03-08 21:23:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post("/api/voice", async (req, res) => {
     const text = req.body.text;
 
     const response = await fetch(
-  "https://api.elevenlabs.io/v1/text-to-speech/ErXwobaYiN019PkySvjV",
+  "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM",
   {
     method: "POST",
     headers: {
@@ -122,4 +122,5 @@ app.post("/api/voice", async (req, res) => {
 // INICIAR SERVIDOR
 app.listen(port, () => {
   console.log("Servidor activo en http://localhost:3000");
+
 });
